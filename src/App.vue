2d82471b8e7cc84b7eead91e7df0d8586cfc9b10
Carl Hain <crhain@gmail.com>
2020-03-23 23:10:14 -0500

    Set up data and form for app

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Animations</h1>
-
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" class="form-control" />
         </div>
+        <div class="form-group">
+          <label>Mail</label>
+          <input type="text" class="form-control" />
+        </div>
+        <button class="btn btn-primary">Submit</button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-
-            }
-        }
-    }
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        email: ""
+      }
+    };
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
