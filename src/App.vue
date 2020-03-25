ee918adaa4e72e506f90123f2631f0b9a0445dab
Carl Hain <crhain@gmail.com>
2020-03-24 19:23:45 -0500

    Add input box to select where data is retrieved from

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@
         </div>
         <button class="btn btn-primary" @click="submit">Submit</button>
         <hr />
+        <input type="text" class="form-control" v-model="node" />
+        <br /><br />
         <button class="btn btn-primary" @click="fetchData">Get Data</button>
         <hr />
         <ul class="list-group">
@@ -34,7 +36,8 @@ export default {
         email: ""
       },
       users: [],
-      resource: {}
+      resource: {},
+      node: "data"
     };
   },
   methods: {
@@ -51,8 +54,18 @@ export default {
       // this.resource.saveAlt(this.user);
     },
     fetchData() {
-      this.$http
-        .get("data.json")
+      // this.$http
+      //   .get("data.json")
+      //   .then(response => response.json())
+      //   .then(data => {
+      //     const resultArray = [];
+      //     for (let key in data) {
+      //       resultArray.push(data[key]);
+      //     }
+      //     this.users = resultArray;
+      //   });
+      this.resource
+        .getData({ node: this.node })
         .then(response => response.json())
         .then(data => {
           const resultArray = [];
@@ -65,9 +78,10 @@ export default {
   },
   created() {
     const customActions = {
-      saveAlt: { method: "POST", url: "alternative.json" }
+      saveAlt: { method: "POST", url: "alternative.json" },
+      getData: { method: "GET", url: "{node}.json" }
     };
-    this.resource = this.$resource("data.json", {}, customActions);
+    this.resource = this.$resource("{node}.json", {}, customActions);
   }
 };
 </script>
